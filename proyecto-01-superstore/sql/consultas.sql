c9c15d7a70c7242e961f79ec9ad5b75f9bacda4d
brunorivsanchez-eng <bruno.riv.sanchez@gmail.com>
2026-08-19 17:48:33 -0400

    Update consultas.sql

--- proyecto-01-superstore/sql/consultas.sql
+++ proyecto-01-superstore/sql/consultas.sql
@@ -1,3 +1,27 @@
+CREATE TABLE staging_superstore (
+    row_id INTEGER,
+    order_id VARCHAR(50),
+    order_date DATE,
+    ship_date DATE,
+    ship_mode VARCHAR(50),
+    customer_id VARCHAR(50),
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    country_region VARCHAR(100),
+    city VARCHAR(100),
+    state_province VARCHAR(100),
+    postal_code VARCHAR(20),
+    region VARCHAR(50),
+    product_id VARCHAR(50),
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    product_name VARCHAR(255),
+    sales NUMERIC(12,2),
+    quantity INTEGER,
+    discount NUMERIC(5,2),
+    profit NUMERIC(12,2)
+);
+
 CREATE TABLE Cliente (
     customer_id VARCHAR(20) PRIMARY KEY,
     customer_name VARCHAR(100),
@@ -41,4 +65,3 @@ FROM staging_superstore;
 INSERT INTO Producto (product_id, category, sub_category, product_name)
 SELECT DISTINCT product_id, category, sub_category, product_name
 FROM staging_superstore;
-
